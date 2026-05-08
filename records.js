5c6d38a9a8d415863cd611103380abfa09da09c3
BIPHMusic <135597439+BIPHMusic@users.noreply.github.com>
2026-05-08 22:15:20 +0800

    Add files via upload

--- records.js
+++ records.js
@@ -58,7 +58,146 @@ const records = [
       "Legato",
       "Theme"
     ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "06:10 PM",
+    "percentage": 10,
+    "exercise": "Vocab Review",
+    "termsToReview": [
+      "Basso Continuo",
+      "Duet",
+      "Figured Bass",
+      "Forte Piano",
+      "Fragmentation",
+      "Legato",
+      "Moderato",
+      "Relative Minor",
+      "Sharp"
+    ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "06:26 PM",
+    "percentage": 10,
+    "exercise": "Vocab",
+    "termsToReview": [
+      "Arpeggiating 64",
+      "Articulation",
+      "Cross Rhythm",
+      "Glissando",
+      "Key",
+      "Perfect 5",
+      "Quadruple Pulse",
+      "Quartet",
+      "Sharp"
+    ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "07:32 PM",
+    "percentage": 10,
+    "exercise": "Vocab",
+    "termsToReview": [
+      "Conjunct Motion",
+      "Hemiola",
+      "Intonation",
+      "Monophonic",
+      "Ostinato",
+      "Pentatonic Scale",
+      "Perfect 4",
+      "Real Sequence",
+      "Ritardano"
+    ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "08:40 PM",
+    "percentage": 0,
+    "exercise": "Vocab",
+    "termsToReview": [
+      "Accented (NCT)",
+      "Appoggiatura",
+      "Consonance",
+      "Harmonic Minor Scale",
+      "Italian Aug 6",
+      "Mixolydian",
+      "Phrygian Half Cadence",
+      "Rhythmic Diminution",
+      "Secondary Leading Tone",
+      "Tritone"
+    ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "08:47 PM",
+    "percentage": 20,
+    "exercise": "Vocab",
+    "termsToReview": [
+      "Accidental",
+      "Countermelody",
+      "Disjunct Motion",
+      "Major 7",
+      "Natural",
+      "Natural Minor Scale",
+      "Octatonic Scale",
+      "Root"
+    ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "08:50 PM",
+    "percentage": 100,
+    "exercise": "Vocab",
+    "termsToReview": []
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "08:59 PM",
+    "percentage": 30,
+    "exercise": "Vocab",
+    "termsToReview": [
+      "Contrary Motion",
+      "Melodic Retrograde",
+      "Mixolydian",
+      "Ornaments",
+      "Plagal Cadence",
+      "Rhythmic Diminution",
+      "Voice Leading"
+    ]
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "09:19 PM",
+    "percentage": 100,
+    "exercise": "Vocab",
+    "termsToReview": []
+  },
+  {
+    "student": "Jim",
+    "date": "2026-05-08",
+    "time": "09:47 PM",
+    "percentage": 0,
+    "exercise": "Vocab",
+    "termsToReview": [
+      "Chromatic",
+      "Compound Meter",
+      "Direct or Hidden 5ths",
+      "Leitmotif",
+      "Passing Tone",
+      "Pedal Point or Tone",
+      "Relative Minor",
+      "Subdominant",
+      "Tendency Tones",
+      "Whole Tone Scale"
+    ]
   }
 ];
-
-export default records;
