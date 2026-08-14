c577813b903864b30910b125a596cd0a2e52f6ba
BIPHMusic <135597439+BIPHMusic@users.noreply.github.com>
2026-08-13 22:54:07 -0700

    Add files via upload

--- apstudents.js
+++ apstudents.js
@@ -1,9 +1,10 @@
 const apStudents = [
     //All AP Students
-    {"name": "Jim", "liveschoolName": "Jim Jim Jim", "house": "Red", "birthdate": "2001-04-21", "altName": "Jim"},
-    {"name": "Jimmy", "liveschoolName": "Jimmy Jimmy Jimmy", "house": "Yellow", "birthdate": "2000-03-02", "altName": "Jimmy"},
-    {"name": "Jimmery", "liveschoolName": "Jimmery Jimmery Jimmery", "house": "Red", "birthdate": "2002-06-29", "altName": "Jimmery"},
-    {"name": "Jimboree", "liveschoolName": "Jimboree Jimboree Jimboree", "house": "Red", "birthdate": "2002-06-29", "altName": "Jimboree"},
-    {"name": "Jimtastic", "liveschoolName": "Jimtastic Jimtastic Jimtastic", "house": "Yellow", "birthdate": "2001-09-19", "altName": "Jimtastic"},
-    {"name": "Jimalicious", "liveschoolName": "Jimalicious Jimalicious Jimalicious", "house": "Blue", "birthdate": "2001-05-09", "altName": "Jimalicious"},
+    {"name": "Simone", "liveschoolName": "Simon Zimo Ma", "house": "Blue", "birthdate": "", "altName": "Simone"},
+    {"name": "Thomas", "liveschoolName": "Thomas Hongyi Long", "house": "Red", "birthdate": "", "altName": "Thomas"},
+    {"name": "Dora", "liveschoolName": "Dora Ziqian Zhang", "house": "Yellow", "birthdate": "", "altName": "Dora"},
+    {"name": "Calvin", "liveschoolName": "Calvin Zihao Zhang", "house": "Blue", "birthdate": "", "altName": "Calvin"},
+    {"name": "William", "liveschoolName": "William Zhiwen Zheng", "house": "Yellow", "birthdate": "", "altName": "William"},
+    {"name": "Jocelyn", "liveschoolName": "Jocelyn Lan Bai", "house": "Blue", "birthdate": "", "altName": "Jocelyn"},
+    {"name": "Teresa", "liveschoolName": "Teresa Mingxun Jiang", "house": "Green", "birthdate": "", "altName": "Teresa"},
 ]
