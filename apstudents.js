63658c981b726a20c770cd06e8938cf7c457584d
BIPHMusic <135597439+BIPHMusic@users.noreply.github.com>
2026-08-19 04:52:22 -0700

    Add files via upload

--- apstudents.js
+++ apstudents.js
@@ -1,10 +1,9 @@
 const apStudents = [
     //All AP Students
-    {"name": "Simone", "liveschoolName": "Simon Zimo Ma", "house": "Blue", "birthdate": "", "altName": "Simone"},
     {"name": "Thomas", "liveschoolName": "Thomas Hongyi Long", "house": "Red", "birthdate": "", "altName": "Thomas"},
-    {"name": "Dora", "liveschoolName": "Dora Ziqian Zhang", "house": "Yellow", "birthdate": "", "altName": "Dora"},
     {"name": "Calvin", "liveschoolName": "Calvin Zihao Zhang", "house": "Blue", "birthdate": "", "altName": "Calvin"},
     {"name": "William", "liveschoolName": "William Zhiwen Zheng", "house": "Yellow", "birthdate": "", "altName": "William"},
     {"name": "Jocelyn", "liveschoolName": "Jocelyn Lan Bai", "house": "Blue", "birthdate": "", "altName": "Jocelyn"},
+    {"name": "Maggie", "liveschoolName": "Maggie Gan", "house": "Red", "birthdate": "", "altName": "Maggie"},
     {"name": "Teresa", "liveschoolName": "Teresa Mingxun Jiang", "house": "Green", "birthdate": "", "altName": "Teresa"},
 ]
