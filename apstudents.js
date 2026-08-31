e35d3d60960df63397d323dbca0494500d0d4314
BIPHMusic <135597439+BIPHMusic@users.noreply.github.com>
2026-08-30 18:33:34 -0700

    Add files via upload

--- apstudents.js
+++ apstudents.js
@@ -1,9 +1,10 @@
 const apStudents = [
     //All AP Students
-    {"name": "Thomas", "liveschoolName": "Thomas Hongyi Long", "house": "Red", "birthdate": "", "altName": "Thomas"},
-    {"name": "Calvin", "liveschoolName": "Calvin Zihao Zhang", "house": "Blue", "birthdate": "", "altName": "Calvin"},
-    {"name": "William", "liveschoolName": "William Zhiwen Zheng", "house": "Yellow", "birthdate": "", "altName": "William"},
-    {"name": "Jocelyn", "liveschoolName": "Jocelyn Lan Bai", "house": "Blue", "birthdate": "", "altName": "Jocelyn"},
-    {"name": "Maggie", "liveschoolName": "Maggie Gan", "house": "Red", "birthdate": "", "altName": "Maggie"},
-    {"name": "Teresa", "liveschoolName": "Teresa Mingxun Jiang", "house": "Green", "birthdate": "", "altName": "Teresa"},
+    {"name": "Jocelyn", "liveschoolName": "Jocelyn Lan Bai", "house": "Blue", "birthdate": "2011-01-19", "altName": "Jocelyn", "class": "AP Music Theory", "addable": "no"},
+    {"name": "Maggie", "liveschoolName": "Maggie Gan", "house": "Red", "birthdate": "2011-02-12", "altName": "Maggie", "class": "AP Music Theory", "addable": "no"},
+    {"name": "Alex", "liveschoolName": "Alex Tsz To Huang", "house": "Blue", "birthdate": "2011-10-19", "altName": "Alex", "class": "AP Music Theory", "addable": "no"},
+    {"name": "Teresa", "liveschoolName": "Teresa Mingxun Jiang", "house": "Green", "birthdate": "2011-02-10", "altName": "Teresa", "class": "AP Music Theory", "addable": "no"},
+    {"name": "Thomas", "liveschoolName": "Thomas Hongyi Long", "house": "Red", "birthdate": "2008-02-12", "altName": "Thomas", "class": "AP Music Theory", "addable": "no"},
+    {"name": "Calvin", "liveschoolName": "Calvin Zihao Zhang", "house": "Blue", "birthdate": "2010-04-21", "altName": "Calvin", "class": "AP Music Theory", "addable": "no"},
+    {"name": "William", "liveschoolName": "William Zhiwen Zheng", "house": "Yellow", "birthdate": "2010-11-21", "altName": "William", "class": "AP Music Theory", "addable": "no"},
 ]
